a21cbe87e29545e4a017974b781b283408338c7a
sheshan <sheshan@wso2telco.com>
2016-05-03 16:55:03 +0530

    convert the workflow-manager into component and create feature PRODEV-349

--- components/workflow-manager/src/main/java/com/wso2telco/dep/internal/Util.java
+++ components/workflow-manager/src/main/java/com/wso2telco/dep/internal/Util.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  ******************************************************************************/
-package com.wso2telco.internal;
+package com.wso2telco.dep.internal;
 
 import org.wso2.carbon.user.core.service.RealmService;
 
