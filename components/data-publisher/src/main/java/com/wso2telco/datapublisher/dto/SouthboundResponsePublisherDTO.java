a22ee3eb07f8637f7e38c638a91ac768bb8cd621
Shahintha <shahintha@wso2telco.com>
2016-08-19 18:21:19 +0530

    12f8add59af5d48cca9414a2fc1971b448131760 SMS MO - additional columns for Txn logs I-019249 - 074 patch

--- components/data-publisher/src/main/java/com/wso2telco/datapublisher/dto/SouthboundResponsePublisherDTO.java
+++ components/data-publisher/src/main/java/com/wso2telco/datapublisher/dto/SouthboundResponsePublisherDTO.java
@@ -525,14 +525,15 @@ public class SouthboundResponsePublisherDTO extends ResponsePublisherDTO {
                 + "          {'name':'ussdSessionId','type':'STRING'},"
                 + "          {'name':'transactionOperationStatus','type':'STRING'},"
                 + "          {'name':'referenceCode','type':'STRING'},"
-                + "          {'name':'spConsumerKey','type':'STRING'}"
+                + "          {'name':'spConsumerKey','type':'STRING'},"
                 
-          	/*  + "          {'name':'destinationAddress','type':'STRING'},"
+          	  	+ "          {'name':'destinationAddress','type':'STRING'},"
                 + "          {'name':'senderAddress','type':'STRING'},"
                 + "          {'name':'message','type':'STRING'},"
                 + "          {'name':'date_Time','type':'STRING'},"
                 + "          {'name':'resourceURL','type':'STRING'},"
-               	+ "          {'name':'messageId','type':'STRING'}"*/
+              //+ "          {'name':'messageId','type':'STRING'},"
+              	+ "          {'name':'message_Id','type':'STRING'}"
                 + "  ]"
                 + "}";
 
