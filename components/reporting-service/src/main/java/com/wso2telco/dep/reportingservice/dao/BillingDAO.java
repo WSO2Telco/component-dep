da19a1464dde1bf107f560b111062fccc8b897cb
Shahintha <shahintha@wso2telco.com>
2016-08-23 09:49:08 +0530

    ab8ee7632d542793b551bd07f8e617e5d9f7f50a jsonBody msisdn filter issue corrected SLA iTop 17513

--- components/reporting-service/src/main/java/com/wso2telco/dep/reportingservice/dao/BillingDAO.java
+++ components/reporting-service/src/main/java/com/wso2telco/dep/reportingservice/dao/BillingDAO.java
@@ -1528,7 +1528,7 @@ public class BillingDAO {
 			.append( " x WHERE operatorId LIKE ? AND replace(userid,'@carbon.super','') LIKE ? AND api LIKE ? AND consumerKey LIKE ? AND STR_TO_DATE(x.time,'%Y-%m-%d') between STR_TO_DATE(?,'%Y-%m-%d') and STR_TO_DATE(?,'%Y-%m-%d') ");			 
 		}
 		if (!msisdn.isEmpty()) {
-			sql.append("AND msisdn LIKE ? or (msisdn LIKE ? or x.jsonbody like '%senderAddress\":\"")
+			sql.append("AND msisdn LIKE ? or (msisdn LIKE ? or x.jsonBody like '%senderAddress\":\"")
 			.append(msisdn)
 			.append("%')");
 		}
