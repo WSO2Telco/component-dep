fd5f8cc3440d971133eb4c610a087e2d603584d0
tharsiga@wso2telco.com <tharsiga@wso2telco.com>
2016-08-30 18:36:58 +0530

    Revert "HUBDEV-376 Add/Delete blacklist numbers"
    This reverts commit 62c400499ee2464dc63dbf9be24dc3d34d808d14.

--- components/operator-service/src/main/java/com/wso2telco/dep/operatorservice/dao/BlackListWhiteListDAO.java
+++ components/operator-service/src/main/java/com/wso2telco/dep/operatorservice/dao/BlackListWhiteListDAO.java
@@ -39,10 +39,10 @@ public class BlackListWhiteListDAO {
 				+ apiID + " apiName:" + apiName + " userID:" + userID);
 
 		StringBuilder sql = new StringBuilder();
-		sql.append(" INSERT INTO ");
+		sql.append(" INSERT INTO `");
 		sql.append(OparatorTable.BLACKLIST_MSISDN.getTObject());
 		sql.append("(MSISDN,API_ID,API_NAME,USER_ID)");
-		sql.append(" VALUES (?, ?, ?, ?)");
+		sql.append("	VALUES (?,?,?,?);");
 
 		Connection conn = null;
 		PreparedStatement ps = null;
@@ -151,35 +151,24 @@ public class BlackListWhiteListDAO {
 
 	public void removeBlacklist(final String apiName, final String userMSISDN) throws SQLException, Exception {
 
-		StringBuilder sql = new StringBuilder();
-		sql.append("DELETE FROM ");
-		sql.append(OparatorTable.BLACKLIST_MSISDN.getTObject());
-		sql.append(" WHERE API_NAME = ?");
-		sql.append(" AND MSISDN = ?");
-		//sql.append(" (API_NAME, MSISDN) ");
-		//sql.append("VALUES (?, ?)");
-
-		
-		
-		//String sql = "DELETE FROM `blacklistmsisdn` WHERE `API_NAME`=? && `MSISDN`=?;";
+		String sql = "DELETE FROM `blacklistmsisdn` WHERE `API_NAME`=? && `MSISDN`=?;";
 
 		Connection conn = null;
 		PreparedStatement ps = null;
-		ResultSet rs = null;
+
 		try {
 			conn = DbUtils.getDbConnection(DataSourceNames.WSO2AM_STATS_DB);
-			ps = conn.prepareStatement(sql.toString());
+			ps = conn.prepareStatement(sql);
 
 			ps.setString(1, apiName);
 			ps.setString(2, userMSISDN);
 
-			ps.executeUpdate();
-							
+			ps.execute();
 		} catch (SQLException e) {
 			throw e;
 		} finally {
 
-			DbUtils.closeAllConnections(ps, conn, rs);
+			DbUtils.closeAllConnections(ps, conn, null);
 
 		}
 	}
@@ -194,7 +183,7 @@ public class BlackListWhiteListDAO {
 	 * @throws SQLException
 	 * @throws Exception
 	 */
-	public void whitelist(List<MSISDN> userMSISDNs, String SubscriptionID, String apiID, String applicationID) 	throws SQLException, Exception {
+	public void whitelist(List<MSISDN> userMSISDNs, String SubscriptionID, String apiID, String applicationID) 			throws SQLException, Exception {
 
 		StringBuilder sql = new StringBuilder();
 		sql.append("INSERT INTO ");
