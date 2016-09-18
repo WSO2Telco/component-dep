b70dabba12004721c381b53d15e0daff3ebcf000
Shashika Wijayasekera <shashika@wso2telco.com>
2016-09-18 18:17:52 +0530

    HUBDEV-648
    Remove the operator names in dep_db.sql and MobileCountryConfig.xml.

--- features/com.wso2telco.dep.hub.core.feature/src/main/resources/sql/mysql/dep_db.sql
+++ features/com.wso2telco.dep.hub.core.feature/src/main/resources/sql/mysql/dep_db.sql
@@ -1,8 +1,8 @@
 /*
 * ==========================================
 * MySQL script related to tables in
-* Axiata database
-* Database: Axiata database
+* DEP database
+* Database: Digital Enable Platform database
 * ==========================================
 */
 
@@ -134,16 +134,16 @@ CREATE TABLE IF NOT EXISTS `subscriptions` (
 * Table for subscription approval operator list.
 */
 CREATE TABLE IF NOT EXISTS `sub_approval_operators` (
-  `API_NAME` varchar(200) DEFAULT NULL,
-  `API_VERSION` varchar(30) DEFAULT NULL,
-  `API_PROVIDER` varchar(200) DEFAULT NULL,
-  `APP_ID` int(11) DEFAULT NULL,
+  `API_NAME` varchar(200) NOT NULL,
+  `API_VERSION` varchar(30) NOT NULL,
+  `API_PROVIDER` varchar(200) NOT NULL,
+  `APP_ID` int(11) NOT NULL,
   `OPERATOR_LIST` varchar(512) DEFAULT NULL,
   PRIMARY KEY (`API_NAME`, `API_VERSION`, `API_PROVIDER`, `APP_ID`)
 );
 
 /*
-* Table for validator definitions used in AxiataMediator
+* Table for validator definitions used in Mediator
 */
 CREATE TABLE IF NOT EXISTS validator (
     id INT NOT NULL AUTO_INCREMENT,
@@ -301,9 +301,9 @@ insert into validator (name, class) values ('msisdn','com.wso2telco.dep.subscrip
 * Default operators
 */
 INSERT INTO `operators` (`ID`, `operatorname`, `description`, `created`, `created_date`, `lastupdated`, `lastupdated_date`, `refreshtoken`, `tokenvalidity`, `tokentime`, `token`, `tokenurl`, `tokenauth`) VALUES
-  (1, 'DIALOG', 'Dialog Opearator', 'axatauser', NULL, 'axatauser', NULL, 'gGgvUANAGhRUzWTyXwYoGuk3WzQa', 157680000, 1395135145139, '4fb164d70def9f37b2f8e2f1daf467', 'http://localhost:8281/token', 'Basic U1JObDQzXzRTVks5MjZaVnNteXExOU1JNVFRYTpEV1Flb2NDeUVyN0lHYk8zRHJxRDc5SmtzVFVh'),
-  (2, 'CELCOM', 'Celcom Opearator', 'axatauser', NULL, 'axatauser', NULL, 'gGgvUANAGhRUzWTyXwYoGuk3WzQa', 157680000, 1395135145139, '4fb164d70def9f37b2f8e2f1daf467', 'http://localhost:8281/token', 'Basic U1JObDQzXzRTVks5MjZaVnNteXExOU1JNVFRYTpEV1Flb2NDeUVyN0lHYk8zRHJxRDc5SmtzVFVh'),
-  (3, 'ROBI', 'Robi Opearator', 'axatauser', NULL, 'axatauser', NULL, 'gGgvUANAGhRUzWTyXwYoGuk3WzQa', 157680000, 1395135145139, '4fb164d70def9f37b2f8e2f1daf467', 'http://localhost:8281/token', 'Basic U1JObDQzXzRTVks5MjZaVnNteXExOU1JNVFRYTpEV1Flb2NDeUVyN0lHYk8zRHJxRDc5SmtzVFVh');
+  (1, 'OPERATOR1', 'Opearator 1', 'depuser', NULL, 'depuser', NULL, 'gGgvUANAGhRUzWTyXwYoGuk3WzQa', 157680000, 1395135145139, '4fb164d70def9f37b2f8e2f1daf467', 'http://localhost:8281/token', 'Basic U1JObDQzXzRTVks5MjZaVnNteXExOU1JNVFRYTpEV1Flb2NDeUVyN0lHYk8zRHJxRDc5SmtzVFVh'),
+  (2, 'OPERATOR2', 'Opearator 2', 'depuser', NULL, 'depuser', NULL, 'gGgvUANAGhRUzWTyXwYoGuk3WzQa', 157680000, 1395135145139, '4fb164d70def9f37b2f8e2f1daf467', 'http://localhost:8281/token', 'Basic U1JObDQzXzRTVks5MjZaVnNteXExOU1JNVFRYTpEV1Flb2NDeUVyN0lHYk8zRHJxRDc5SmtzVFVh'),
+  (3, 'OPERATOR3', 'Opearator 3', 'depuser', NULL, 'depuser', NULL, 'gGgvUANAGhRUzWTyXwYoGuk3WzQa', 157680000, 1395135145139, '4fb164d70def9f37b2f8e2f1daf467', 'http://localhost:8281/token', 'Basic U1JObDQzXzRTVks5MjZaVnNteXExOU1JNVFRYTpEV1Flb2NDeUVyN0lHYk8zRHJxRDc5SmtzVFVh');
 
 /*
 * Default operator endpoints point to the sandbox
@@ -311,7 +311,7 @@ INSERT INTO `operators` (`ID`, `operatorname`, `description`, `created`, `create
 INSERT INTO `operatorendpoints` (`ID`, `operatorid`, `api`, `isactive`, `endpoint`, `created`, `created_date`, `lastupdated`, `lastupdated_date`) VALUES
   (1, 1, 'smsmessaging', 1, 'http://localhost:8081/mifesandbox/SandboxController/smsmessaging/1', NULL, '2014-03-04 11:36:23', NULL, NULL),
   (2, 1, 'payment', 1, 'http://localhost:8081/mifesandbox/SandboxController/payment/1', NULL, '2014-03-04 11:36:58', NULL, NULL),
-  (3, 3, 'location', 1, 'http://localhost:8081/mifesandbox/SandboxController/location/1', NULL, '2014-10-02 10:33:41', NULL, NULL);
+  (3, 1, 'location', 1, 'http://localhost:8081/mifesandbox/SandboxController/location/1', NULL, '2014-10-02 10:33:41', NULL, NULL);
 
 --
 -- Initial Metadata script for table `workflow_api_key_mappings`
