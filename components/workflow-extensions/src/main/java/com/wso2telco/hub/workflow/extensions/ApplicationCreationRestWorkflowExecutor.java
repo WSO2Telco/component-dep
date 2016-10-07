dcd0a67b0f36993902a8f0b83bd21e5f26a3731d
thilinicooray <thilinicooray.ucsc@gmail.com>
2016-10-07 17:16:38 +0530

    Fixing PR suggestions

---
+++ components/workflow-extensions/src/main/java/com/wso2telco/hub/workflow/extensions/ApplicationCreationRestWorkflowExecutor.java
@@ -0,0 +1,7 @@
+package com.wso2telco.hub.workflow.extensions;
+
+/**
+ * Created by thilinicooray on 10/7/16.
+ */
+public class ApplicationCreationRestWorkflowExecutor {
+}
---
+++ components/workflow-extensions/src/main/java/com/wso2telco/hub/workflow/extensions/util/DeploymentTypes.java
@@ -0,0 +1,7 @@
+package com.wso2telco.hub.workflow.extensions.util;
+
+/**
+ * Created by thilinicooray on 10/7/16.
+ */
+public class DeploymentTypes {
+}
