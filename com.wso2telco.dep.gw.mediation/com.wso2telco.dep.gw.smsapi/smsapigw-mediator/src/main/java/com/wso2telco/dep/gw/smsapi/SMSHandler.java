7b07c1f2e5e30d79d5de30d5a504760769f8bc1c
manoj <manojg@wwso2.com>
2016-12-14 18:19:45 +0530

    Initialize new project staructure

---
+++ com.wso2telco.dep.gw.mediation/com.wso2telco.dep.gw.smsapi/smsapigw-mediator/src/main/java/com/wso2telco/dep/gw/smsapi/SMSHandler.java
@@ -0,0 +1,12 @@
+package com.wso2telco.dep.gw.smsapi;
+
+import org.apache.synapse.MessageContext; 
+import org.apache.synapse.mediators.AbstractMediator;
+
+public class SMSHandler extends AbstractMediator { 
+
+	public boolean mediate(MessageContext context) { 
+		// TODO Implement your mediation logic here 
+		return true;
+	}
+}
---
+++ com.wso2telco.dep.gw.mediation/com.wso2telco.dep.gw.ussdapi/ussdapigw-mediator/src/main/java/com/wso2telco/dep/gw/ussdapi/USSDHandler.java
@@ -0,0 +1,12 @@
+package com.wso2telco.dep.gw.ussdapi;
+
+import org.apache.synapse.MessageContext; 
+import org.apache.synapse.mediators.AbstractMediator;
+
+public class USSDHandler extends AbstractMediator { 
+
+	public boolean mediate(MessageContext context) { 
+		// TODO Implement your mediation logic here 
+		return true;
+	}
+}
