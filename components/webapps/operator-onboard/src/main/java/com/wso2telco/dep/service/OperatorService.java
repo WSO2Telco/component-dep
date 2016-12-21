feb7d986a5209793b94988ca624b63fb28f44f28
tharsigam <tharsiga@wso2telco.com>
2016-12-21 12:56:29 +0530

    HUBDEV-1218 - Operator Onboard_Initial Project Structure

---
+++ components/webapps/operator-onboard/src/main/java/com/wso2telco/dep/service/OperatorService.java
@@ -0,0 +1,39 @@
+package com.wso2telco.dep.service;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
+
+import org.apache.log4j.Logger;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.wso2telco.dep.model.AddOperator;
+
+@Path("/operator/v1")
+public class OperatorService {
+	
+	private static final Logger LOG = Logger.getLogger(OperatorService.class.getName());
+	
+	@Context
+	private UriInfo context;
+	
+	@POST
+	@Path("/add	")
+	@Consumes("application/json")
+	@Produces("application/json")	
+	public Response addOperator(String jsonBody) throws Exception{
+		LOG.debug("addOperator Triggerd  jsonBody :");
+		//add operator goes here
+		Gson gson = new GsonBuilder().serializeNulls().create();
+		AddOperator addOperator = gson.fromJson(jsonBody,AddOperator.class);
+		
+		//rest of the implementation goes here
+		
+		return null;	
+	}
+}
