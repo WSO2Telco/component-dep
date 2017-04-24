806e9bed2aec2d84c09a494dd71c86fe2a679c27
shawijayasekera <shashika@wso2telco.com>
2017-04-24 16:43:40 +0530

    Add billing-service module.

---
+++ components/billing/billing-service/src/main/java/com/wso2telco/dep/billingservice/service/RateCardService.java
@@ -0,0 +1,100 @@
+package com.wso2telco.dep.billingservice.service;
+
+import java.util.Collections;
+import java.util.Map;
+import com.wso2telco.dep.billingservice.dao.RateCardDAO;
+
+public class RateCardService {
+
+	RateCardDAO rateCardDAO;
+
+	{
+		rateCardDAO = new RateCardDAO();
+	}
+
+	public Map<Integer, String> getServiceDetailsByAPICode(String apiCode) throws Exception {
+
+		Map<Integer, String> serviceDetails = null;
+
+		try {
+
+			serviceDetails = rateCardDAO.getServiceDetailsByAPICode(apiCode);
+		} catch (Exception e) {
+
+			throw e;
+		}
+
+		if (serviceDetails != null) {
+
+			return serviceDetails;
+		} else {
+
+			return Collections.emptyMap();
+		}
+	}
+
+	public Map<Integer, String> getHubRateDetailsByServicesDid(int servicesDid) throws Exception {
+
+		Map<Integer, String> rateDetails = null;
+
+		try {
+
+			rateDetails = rateCardDAO.getHubRateDetailsByServicesDid(servicesDid);
+		} catch (Exception e) {
+
+			throw e;
+		}
+
+		if (rateDetails != null) {
+
+			return rateDetails;
+		} else {
+
+			return Collections.emptyMap();
+		}
+	}
+
+	public Map<Integer, String> getOperatorRateDetailsByServicesDidAndOperatorCode(int servicesDid, String operatorCode)
+			throws Exception {
+
+		Map<Integer, String> rateDetails = null;
+
+		try {
+
+			rateDetails = rateCardDAO.getOperatorRateDetailsByServicesDidAndOperatorCode(servicesDid, operatorCode);
+		} catch (Exception e) {
+
+			throw e;
+		}
+
+		if (rateDetails != null) {
+
+			return rateDetails;
+		} else {
+
+			return Collections.emptyMap();
+		}
+	}
+
+	public void setHubSubscriptionRateData(int servicesRateDid, int applicationDid, String apiCode) throws Exception {
+
+		try {
+
+			rateCardDAO.setHubSubscriptionRateData(servicesRateDid, applicationDid, apiCode);
+		} catch (Exception e) {
+
+			throw e;
+		}
+	}
+
+	public void setOperatorSubscriptionRateData(int operatorRateDid, int applicationDid) throws Exception {
+
+		try {
+
+			rateCardDAO.setOperatorSubscriptionRateData(operatorRateDid, applicationDid);
+		} catch (Exception e) {
+
+			throw e;
+		}
+	}
+}
