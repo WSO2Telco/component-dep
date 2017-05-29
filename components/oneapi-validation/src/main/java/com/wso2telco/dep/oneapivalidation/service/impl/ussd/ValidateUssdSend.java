04ba42e274623b485a2e0ecd4b8bff8e9c847ada
Buddhima <buddhimawijeweera@yahoo.com>
2017-05-29 12:04:35 +0530

    make notifyURL mandatory

--- components/oneapi-validation/src/main/java/com/wso2telco/dep/oneapivalidation/service/impl/ussd/ValidateUssdSend.java
+++ components/oneapi-validation/src/main/java/com/wso2telco/dep/oneapivalidation/service/impl/ussd/ValidateUssdSend.java
@@ -92,7 +92,7 @@ public class ValidateUssdSend implements IServiceValidate {
             new ValidationRule(ValidationRule.VALIDATION_TYPE_MANDATORY, "outboundUSSDMessage", message),
             new ValidationRule(ValidationRule.VALIDATION_TYPE_OPTIONAL, "clientCorrelator", clientCorrelator),
             new ValidationRule(ValidationRule.VALIDATION_TYPE_MANDATORY, "ussdAction", ussdAction),
-            new ValidationRule(ValidationRule.VALIDATION_TYPE_OPTIONAL, "notifyURL", notifyUrl),
+            new ValidationRule(ValidationRule.VALIDATION_TYPE_MANDATORY, "notifyURL", notifyUrl),
             new ValidationRule(ValidationRule.VALIDATION_TYPE_OPTIONAL, "callbackData", callbackData),};
         Validation.checkRequestParams(rules);
     }
