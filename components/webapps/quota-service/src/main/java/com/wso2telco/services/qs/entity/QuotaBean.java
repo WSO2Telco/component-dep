e7164a51683adb53cfcf7c3e6b7a914707b3d91f
Priyanka <priyankahdp@gmail.com>
2017-07-26 17:28:19 +0530

    quota limit service

---
+++ components/webapps/quota-service/src/main/java/com/wso2telco/services/qs/entity/QuotaBean.java
@@ -0,0 +1,50 @@
+package com.wso2telco.services.qs.entity;
+
+public class QuotaBean {
+	private String operator;
+	private String serviceProvider;
+	private String applicationName;
+	private String apiName;
+	private String quotaLimit;
+
+	public String getOperator() {
+		return operator;
+	}
+
+	public void setOperator(String operator) {
+		this.operator = operator;
+	}
+
+	public String getServiceProvider() {
+		return serviceProvider;
+	}
+
+	public void setServiceProvider(String serviceProvider) {
+		this.serviceProvider = serviceProvider;
+	}
+
+	public String getApplicationName() {
+		return applicationName;
+	}
+
+	public void setApplicationName(String applicationName) {
+		this.applicationName = applicationName;
+	}
+
+	public String getApiName() {
+		return apiName;
+	}
+
+	public void setApiName(String apiName) {
+		this.apiName = apiName;
+	}
+
+	public String getQuotaLimit() {
+		return quotaLimit;
+	}
+
+	public void setQuotaLimit(String quotaLimit) {
+		this.quotaLimit = quotaLimit;
+	}
+
+}
---
+++ components/webapps/quota-service/src/main/java/com/wso2telco/services/qs/service/QuotaLimitService.java
@@ -0,0 +1,57 @@
+package com.wso2telco.services.qs.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.regexp.recompile;
+
+import com.wso2telco.core.dbutils.exception.BusinessException;
+import com.wso2telco.core.dbutils.exception.GenaralError;
+import com.wso2telco.dep.qs.util.QuotaLimitException;
+import com.wso2telco.services.qs.dao.QuotaLimitDao;
+import com.wso2telco.services.qs.entity.QuotaBean;
+
+public class QuotaLimitService {
+
+	Log LOG = LogFactory.getLog(QuotaLimitService.class);
+	private QuotaLimitDao dao;
+
+	{
+		dao = new QuotaLimitDao();
+	}
+
+	public void addQuotaLimit(QuotaBean quotaBean) throws BusinessException {
+		try {
+			dao.addQuotaLimit(quotaBean);
+		} catch (Exception exception) {
+			LOG.error("error in addQuotaLimit", exception);
+			throw new QuotaLimitException(GenaralError.INTERNAL_SERVER_ERROR);
+		}
+	}
+
+	public List<QuotaBean> getQuotaLimitInfo(String byFlag, String info) throws QuotaLimitException{
+		List<QuotaBean> returnObjList=new ArrayList<QuotaBean>();
+		try {
+			switch (byFlag) {
+			case "byServiceProvider":
+				returnObjList= dao.getQuotaLimitInfoByServiceProvider(info);
+				break;
+			case "byApplication":
+				returnObjList= dao.getQuotaLimitInfoByApplication(info);
+				break;
+			case "byApi":
+				returnObjList= dao.getQuotaLimitInfoByApi(info);
+				break;
+			default:
+				break;
+			}
+			return returnObjList;
+		} catch (Exception exception) {
+			LOG.error("error in addQuotaLimit", exception);
+			throw new QuotaLimitException(GenaralError.INTERNAL_SERVER_ERROR);
+		}
+	}
+
+}
