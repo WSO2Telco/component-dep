44ffc9e0c3f46335b4fae4967408817d1d49d42e
Buddhima <buddhimawijeweera@yahoo.com>
2017-09-06 14:20:42 +0530

    adding second method

--- components/webapps/operator-onboard/src/main/java/com/wso2telco/dep/service/OperatorService.java
+++ components/webapps/operator-onboard/src/main/java/com/wso2telco/dep/service/OperatorService.java
@@ -52,4 +52,17 @@ public class OperatorService {
 		}
 
 	}
+
+	public String barMethod(String abc) {
+
+		try {
+			Thread.sleep(1000);
+			FileInputStream file = new FileInputStream("hard_coded_path");
+		} catch (InterruptedException e) {
+
+		} catch (FileNotFoundException e) {
+
+		}
+		return null;
+	}
 }
