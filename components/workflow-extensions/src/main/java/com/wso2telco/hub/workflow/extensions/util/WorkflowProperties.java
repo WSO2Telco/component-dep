f3a5d1327759a091288d00ec14f849ea8f9f075b
Chinthana <chinthana@wso2telco.com>
2017-10-10 14:46:08 +0530

    Changed current workflow.properties to xml - removed unwanted imports

--- components/workflow-extensions/src/main/java/com/wso2telco/hub/workflow/extensions/util/WorkflowProperties.java
+++ components/workflow-extensions/src/main/java/com/wso2telco/hub/workflow/extensions/util/WorkflowProperties.java
@@ -22,20 +22,17 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
 import java.io.File;
-import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
-import org.xml.sax.SAXException;
 
 public class WorkflowProperties {
 
