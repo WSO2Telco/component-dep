c142dd2bbf8b0984cd8ddd022dca4fd7c8560154
Rajithkonara <rajithkonara@gmail.com>
2017-12-07 12:58:12 +0530

    EXTGW-82 - requested changes

--- components/oneapi-validation/src/main/java/com/wso2telco/dep/oneapivalidation/util/MsisdnFormatMediator.java
+++ components/oneapi-validation/src/main/java/com/wso2telco/dep/oneapivalidation/util/MsisdnFormatMediator.java
@@ -7,7 +7,17 @@ import static com.wso2telco.dep.oneapivalidation.util.Validation.getDigitsGroup;
 import static com.wso2telco.dep.oneapivalidation.util.Validation.getPrefixGroup;
 import static com.wso2telco.dep.oneapivalidation.util.Validation.getValidationRegex;
 
-public class RegexMediator extends AbstractMediator {
+public class MsisdnFormatMediator extends AbstractMediator {
+
+    static final String MESSAGE_ID = "messageId";
+    static final String ERROR_TEXT = "errorText";
+    static final String ERROR_VARIABLE = "errorVariable";
+    static final String HTTP_STATUS_CODE = "httpStatusCode";
+    static final String EXCEPTION_TYPE = "exceptionType";
+    static final String VALIDATION_REGEX = "validationRegex";
+    static final String VALIDATION_PREFIX_GROUP = "validationPrefixGroup";
+    static final String VALIDATION_DIGITS_GROUP = "validationDigitsGroup";
+
 
     @Override
     public boolean mediate(MessageContext mc) {
@@ -18,9 +28,9 @@ public class RegexMediator extends AbstractMediator {
 
         try {
 
-            mc.setProperty("validationRegex", validationRegex);
-            mc.setProperty("validationPrefixGroup",prefixGroup);
-            mc.setProperty("validationDigitsGroup",digitsGroup);
+            mc.setProperty(VALIDATION_REGEX, validationRegex);
+            mc.setProperty(VALIDATION_PREFIX_GROUP, prefixGroup);
+            mc.setProperty(VALIDATION_DIGITS_GROUP, digitsGroup);
 
         } catch (Exception e) {
             log.error("Error Returning Validation Regex", e);
@@ -35,11 +45,11 @@ public class RegexMediator extends AbstractMediator {
                                    String errorText, String errorVariable, String httpStatusCode,
                                    String exceptionType) {
 
-        synContext.setProperty("messageId", messageId);
-        synContext.setProperty("errorText", errorText);
-        synContext.setProperty("errorVariable", errorVariable);
-        synContext.setProperty("httpStatusCode", httpStatusCode);
-        synContext.setProperty("exceptionType", exceptionType);
+        synContext.setProperty(MESSAGE_ID, messageId);
+        synContext.setProperty(ERROR_TEXT, errorText);
+        synContext.setProperty(ERROR_VARIABLE, errorVariable);
+        synContext.setProperty(HTTP_STATUS_CODE, httpStatusCode);
+        synContext.setProperty(EXCEPTION_TYPE, exceptionType);
     }
 
 }
