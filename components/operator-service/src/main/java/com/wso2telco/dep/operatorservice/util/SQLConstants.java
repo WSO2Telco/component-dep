eee12e080811cb1e7a392596f8de6a76a248626a
azhamn <azham@wso2telco.com>
2017-12-11 11:53:56 +0530

    deleted commented out lines

--- components/operator-service/src/main/java/com/wso2telco/dep/operatorservice/util/SQLConstants.java
+++ components/operator-service/src/main/java/com/wso2telco/dep/operatorservice/util/SQLConstants.java
@@ -34,17 +34,6 @@ public class SQLConstants {
 					" ORDER BY " +
 					"   APP.NAME";
 
-
-//	public static final String GET_APP_API_USER_SQL = new StringBuilder()
-//			.append(" SELECT distinct(SUB.USER_ID) AS USER_ID FROM AM_SUBSCRIPTION SUBS, AM_APPLICATION APP,  ")
-//			.append("AM_SUBSCRIBER SUB, AM_API API WHERE SUBS.APPLICATION_ID = APP.APPLICATION_ID ")
-//			.append("AND APP.SUBSCRIBER_ID = SUB.SUBSCRIBER_ID AND API.API_ID = SUBS.API_ID ")
-//			.append("AND SUBS.SUB_STATUS != '")
-//			.append(BlacklistWhitelistConstants.SubscriptionStatus.REJECTED)
-//			.append("' ")
-//			.toString();
-
-
 	public static final String GET_KEY_SQL_OF_SUBSCRIPTION_ID_PREFIX =
 			" SELECT " +
 					"   IAT.ACCESS_TOKEN AS ACCESS_TOKEN," +
@@ -111,15 +100,6 @@ public class SQLConstants {
 			"'"+BlacklistWhitelistConstants.SubscriptionStatus.REJECTED +"' AND SUB.USER_ID = ? AND APP.APPLICATION_ID = " +
 			"? ORDER BY APP.NAME";
 
-
-/*	public static final String GET_SUBSCRIPTION_ID_FOR_API_AND_APP_SQL = "SELECT SUBS.SUBSCRIPTION_ID AS SUBSCRIPTION_ID,  " +
-			"SUBS.APPLICATION_ID AS APPLICATION_ID, API.API_PROVIDER AS API_PROVIDER, API.API_NAME AS API_NAME, " +
-			"API.API_VERSION AS API_VERSION, APP.NAME AS APPNAME  FROM AM_SUBSCRIPTION SUBS, AM_APPLICATION APP,  " +
-			"AM_API API WHERE API.API_PROVIDER = ? AND API.API_NAME = ? AND API.API_VERSION = ? " +
-			"AND APP.NAME = ? AND SUBS.APPLICATION_ID = APP.APPLICATION_ID AND API.API_ID = SUBS.API_ID " +
-			"AND SUBS.SUB_STATUS != 'BlacklistWhitelistConstants.SubscriptionStatus.REJECTED ' ORDER BY APP.NAME";*/
-
-
 	public static final String GET_SUBSCRIPTION_ID_FOR_API_AND_APP_SQL = "SELECT SUBS.SUBSCRIPTION_ID AS " +
 			"SUBSCRIPTION_ID FROM AM_SUBSCRIPTION SUBS, AM_APPLICATION APP, AM_API API WHERE API.API_ID = ? " +
 			"AND APP.APPLICATION_ID = ? AND SUBS.APPLICATION_ID = APP.APPLICATION_ID AND API.API_ID = SUBS.API_ID " +
@@ -141,75 +121,5 @@ public class SQLConstants {
 			"SELECT wh.msisdn FROM subscription_whitelist wh,"+DbUtils.getDbNames().get(DataSourceNames.WSO2AM_DB)+".AM_SUBSCRIPTION sub " +
 			"where sub.created_by=? and wh.api_id=? and wh.application_id=? and wh.subscriptionId=sub.subscription_Id ";
 
-//	public static final String GET_SP_NAMES = new StringBuilder().append("SELECT distinct(authz_user) from")
-//			.append("  (SELECT ac.authz_user")
-//			.append("   FROM ")
-//			.append(OparatorTable.IDN_OAUTH2_ACCESS_TOKEN)
-//			.append(" ac, ")
-//			.append(OparatorTable.IDN_OAUTH_CONSUMER_APPS)
-//			.append(" ca, ")
-//			.append(OparatorTable.AM_APPLICATION.getTObject())
-//			.append(" am, ")
-//			.append(OparatorTable.AM_APPLICATION_KEY_MAPPING.getTObject())
-//			.append(" km")
-//			.append("   WHERE ac .CONSUMER_KEY_ID = ca.ID")
-//			.append("     AND km .application_id = am.application_id")
-//			.append("     AND km .consumer_key = ca.consumer_key")
-//			.append("     AND ac .authz_user = ca.username")
-//			.append("     AND user_type = 'APPLICATION'")
-//			.append("     AND token_State = 'Active') AS dummy")
-//			.toString();
-
-//	public static final String GET_ADMIN_USERS = new StringBuilder()
-//			.append("SELECT usr.um_user_name ")
-//			.append("		FROM ").append(OparatorTable.UM_USER.getTObject()).append(" usr, ")
-//			.append(OparatorTable.UM_USER_ROLE.getTObject())
-//			.append(" usr_role ")
-//			.append("WHERE usr.UM_ID = usr_role.UM_USER_ID ")
-//			.append("  AND usr_role.UM_ROLE_ID = ")
-//			.append("    (SELECT role.UM_ID ")
-//			.append("     FROM ")
-//			.append(OparatorTable.UM_ROLE.getTObject())
-//			.append(" role ")
-//			.append("     WHERE role.UM_ROLE_NAME =?)")
-//			.toString();
-
-//	public static final String GET_SP_APPS = new StringBuilder().append("select am.application_id,ca.APP_NAME,ac.authz_user,ac.ACCESS_TOKEN,")
-//			.append("ca.consumer_key,ca.consumer_secret from ")
-//			.append(OparatorTable.IDN_OAUTH2_ACCESS_TOKEN.getTObject())
-//			.append(" ac, ")
-//			.append(OparatorTable.IDN_OAUTH_CONSUMER_APPS.getTObject())
-//			.append(" ca, ")
-//			.append(OparatorTable.AM_APPLICATION.getTObject())
-//			.append(" am, ")
-//			.append(OparatorTable.AM_APPLICATION_KEY_MAPPING)
-//			.append(" km ")
-//			.append("where ac.consumer_key_id=ca.id and km.application_id=am.application_id  ")
-//			.append("and km.consumer_key=ca.consumer_key and ac.authz_user=ca.username and user_type='APPLICATION' ")
-//			.append("and token_State='Active' and authz_user = ?")
-//			.toString();
-
-//	public static final String GET_BLACKLISTED_SP_LIST = new StringBuilder().append("SELECT DISTINCT ( username ) ")
-//			.append("FROM   ((SELECT ca.username ")
-//			.append("         FROM   idn_identity_user_data iud, ")
-//			.append("                idn_oauth_consumer_apps ca ")
-//			.append("         WHERE  data_key = 'http://wso2.org/claims/identity/accountlocked' ")
-//            .append("                AND iud.DATA_VALUE = 'true'                                ")
-//			.append("                AND ca.username = iud.user_name ")
-//			.append("                AND ca.grant_types = '')) AS r ")
-//			.toString();
-
-//	public static final String GET_BLACKLISTED_SP_APPS = new StringBuilder().append("SELECT am.application_id, ")
-//			.append("       ca.app_name, ")
-//			.append("       ca.consumer_key, ")
-//			.append("       ca.consumer_secret ")
-//			.append("FROM   idn_oauth_consumer_apps ca, ")
-//			.append("       am_application am, ")
-//			.append("       am_application_key_mapping km ")
-//			.append("WHERE  km.application_id = am.application_id ")
-//			.append("       AND km.consumer_key = ca.consumer_key ")
-//			.append("       AND ca.grant_types = '' ")
-//			.append("       AND ca.username = ? ")
-//            .toString();
 
 }
