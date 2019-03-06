e46e6ed4de2d4e7c913a1823ed4e41edf35eeb8a
SkNuwanTissera <nuwan.tissera@my.sliit.lk>
2019-03-06 11:12:34 +0530

    INTGW-463 PR incorporation - JS errors

--- components/jaggery-apps/store/src/main/store/site/themes/wso2/js/cbpViewModeSwitch.js
+++ components/jaggery-apps/store/src/main/store/site/themes/wso2/js/cbpViewModeSwitch.js
@@ -10,58 +10,62 @@
  */
 (function() {
 
-	var container = document.getElementById( 'cbp-vm' ),
-        optionSwitch = Array.prototype.slice.call( container.querySelectorAll( 'div.cbp-vm-options > a' ) );
+	var container = document.getElementById( 'cbp-vm' );
 
-    var containerRecentVal = document.getElementById( 'cbp-vm-recent' );
+	if(container !== null) { //to Enable Switch Mode only if the widget presents - API page only
 
-    if(containerRecentVal !== null){
-    	var containerRecent = document.getElementById( 'cbp-vm-recent' ),
-            optionSwitchRecent = Array.prototype.slice.call( containerRecent.querySelectorAll( 'div.cbp-vm-options > a' ) );
-    }
+		var optionSwitch = Array.prototype.slice.call(container.querySelectorAll('div.cbp-vm-options > a'));
 
-	function init() {
-		optionSwitch.forEach( function( el, i ) {
-			el.addEventListener( 'click', function( ev ) {
-				ev.preventDefault();
-				_switch( this );
-			}, false );
-		} );
+		var containerRecentVal = document.getElementById('cbp-vm-recent');
 
-        if(containerRecentVal !== null) {
-            optionSwitchRecent.forEach(function (el, i) {
-                el.addEventListener('click', function (ev) {
-                    ev.preventDefault();
-                    _switchRecent(this);
-                }, false);
-            });
-        }
+		if (containerRecentVal !== null) {
+			var containerRecent = document.getElementById('cbp-vm-recent'),
+				optionSwitchRecent = Array.prototype.slice.call(containerRecent.querySelectorAll('div.cbp-vm-options > a'));
+		}
+
+		function init() {
+			optionSwitch.forEach(function (el, i) {
+				el.addEventListener('click', function (ev) {
+					ev.preventDefault();
+					_switch(this);
+				}, false);
+			});
+
+			if (containerRecentVal !== null) {
+				optionSwitchRecent.forEach(function (el, i) {
+					el.addEventListener('click', function (ev) {
+						ev.preventDefault();
+						_switchRecent(this);
+					}, false);
+				});
+			}
+		}
+
+		function _switch(opt) {
+			// remove other view classes and any any selected option
+			optionSwitch.forEach(function (el) {
+				classie.remove(container, el.getAttribute('data-view'));
+				classie.remove(el, 'cbp-vm-selected');
+			});
+			// add the view class for this option
+			classie.add(container, opt.getAttribute('data-view'));
+			// this option stays selected
+			classie.add(opt, 'cbp-vm-selected');
+		}
+
+		function _switchRecent(opt) {
+			// remove other view classes and any any selected option
+			optionSwitchRecent.forEach(function (el) {
+				classie.remove(containerRecent, el.getAttribute('data-view'));
+				classie.remove(el, 'cbp-vm-selected');
+			});
+			// add the view class for this option
+			classie.add(containerRecent, opt.getAttribute('data-view'));
+			// this option stays selected
+			classie.add(opt, 'cbp-vm-selected');
+		}
+
+		init();
+
 	}
-
-	function _switch( opt ) {
-		// remove other view classes and any any selected option
-		optionSwitch.forEach(function(el) { 
-			classie.remove( container, el.getAttribute( 'data-view' ) );
-			classie.remove( el, 'cbp-vm-selected' );
-		});
-		// add the view class for this option
-		classie.add( container, opt.getAttribute( 'data-view' ) );
-		// this option stays selected
-		classie.add( opt, 'cbp-vm-selected' );
-	}
-
-    function _switchRecent( opt ) {
-        // remove other view classes and any any selected option
-        optionSwitchRecent.forEach(function(el) {
-            classie.remove( containerRecent, el.getAttribute( 'data-view' ) );
-            classie.remove( el, 'cbp-vm-selected' );
-        });
-        // add the view class for this option
-        classie.add( containerRecent, opt.getAttribute( 'data-view' ) );
-        // this option stays selected
-        classie.add( opt, 'cbp-vm-selected' );
-    }
-
-	init();
-
 })();
