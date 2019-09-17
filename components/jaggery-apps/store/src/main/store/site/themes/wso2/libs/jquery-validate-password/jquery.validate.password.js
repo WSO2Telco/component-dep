aaa0d631d60b29e5f6ae7b07e353d7cc67af2910
lahirudulanjaya <laahirudulanjaya@gmail.com>
2019-09-17 12:44:10 +0530

    signup form validation & publisher username

--- components/jaggery-apps/store/src/main/store/site/themes/wso2/libs/jquery-validate-password/jquery.validate.password.js
+++ components/jaggery-apps/store/src/main/store/site/themes/wso2/libs/jquery-validate-password/jquery.validate.password.js
@@ -45,10 +45,11 @@ $.validator.setDefaults({
 	}
 	
 	$.validator.passwordRating = function(password, username) {
-            var minLength = 6;
+			var minLength = 6;
+			var maxLength = 30;
             var passwordStrength   = 0;
 
-			if (password.length >= minLength) passwordStrength++;
+			if (password.length >= minLength && password.length <= maxlength) passwordStrength++;
 
 			if ((password.match(/[a-z]/)) && (password.match(/[A-Z]/)) ) passwordStrength++;
 
@@ -56,7 +57,7 @@ $.validator.setDefaults({
 
 			if (password.match(/.[!,@,#,$,%,^,&,*,?,_,~,-,(,)]/))	passwordStrength++;
 
-			if (password.length > 12) passwordStrength++;
+		//	if (password.length > 12) passwordStrength++;
 
 			if ((password.length >0) && (password.length <=5)) passwordStrength=0;
 
@@ -115,7 +116,7 @@ $.validator.setDefaults({
 		.addClass("password-meter-message-" + rating.messageKey)
 		.text($.validator.passwordRating.messages[rating.messageKey]);
 		// display process bar instead of error message
-		return rating.rate > 2;
+		return rating.rate > 3;
 	}, "Minimum system requirements not met");
 	// manually add class rule, to make username param optional
 	$.validator.classRuleSettings.password = { password: true };
