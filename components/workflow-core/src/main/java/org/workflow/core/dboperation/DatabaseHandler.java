23de5052a0d05b4e8003d6b3eb391546838e7e02
Milinda Laknath Arambawela <milindalaknath@gmail.com>
2019-10-09 15:16:05 +0530

    INTGW-724 | Pagination issue fixed by filtering Default application from DB using SQL query. Performance tuning.

--- components/workflow-core/src/main/java/org/workflow/core/dboperation/DatabaseHandler.java
+++ components/workflow-core/src/main/java/org/workflow/core/dboperation/DatabaseHandler.java
@@ -102,7 +102,7 @@ public class DatabaseHandler {
                 .append("WHERE EXISTS( SELECT 1 FROM " + depDB + "." + Tables.DEP_OPERATOR_APPS.getTObject() + " opcoApp ")
                 .append("INNER JOIN " + depDB + "." + Tables.DEP_OPERATORS.getTObject() + " opco ON opcoApp.operatorid = opco.id ")
                 .append("WHERE opcoApp.isactive LIKE ? AND opcoApp.applicationid = amapp.application_id AND ")
-                .append("opco.operatorname LIKE ? AND amapp.application_id LIKE ? AND amapp.name LIKE ? AND amapp.subscriber_id LIKE ? ) ");
+                .append("opco.operatorname LIKE ? AND amapp.application_id LIKE ? AND amapp.name LIKE ? AND amapp.subscriber_id LIKE ? AND amapp.name!='DefaultApplication' ) ");
 
             if(status!=null && !status.isEmpty()&& !status.equals(ALL)) {
             	sql	.append("AND amapp.application_status LIKE ? ");
@@ -160,11 +160,9 @@ public class DatabaseHandler {
             int size = 0;
             rs = ps.executeQuery();
             while (rs.next()) {
-                /** Does not consider default application */
-                if (!rs.getString("name").equalsIgnoreCase("DefaultApplication")) {
-                    applist.add(new HistoryDetails(rs));
-                    size++;
-                }
+                /** Does not consider default application. Filtered in the query */
+                applist.add(new HistoryDetails(rs));
+                size++;
             }
 
             historyResponse.setApplications(applist);
