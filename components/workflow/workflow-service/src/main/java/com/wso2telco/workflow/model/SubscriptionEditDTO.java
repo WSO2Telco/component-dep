buddhimihara/component-dep-1

[INTGW-1163] Sp Email notification for Tier Changes

components/workflow/workflow-service/src/main/java/com/wso2telco/workflow/model/SubscriptionEditDTO.java
@@ -5,8 +5,11 @@ public class SubscriptionEditDTO {
 	private Integer applicationId;
 	private String applicationName;
 	private String subscriptionTier;
+	private String existingTier;
 	private String apiName;
+	private String apiVersion;
 	private String user;
+	private String sp;
 	private String department;
 
 	public Integer getApplicationId() {
@@ -37,9 +40,17 @@ public class SubscriptionEditDTO {
 		return apiName;
 	}
 
+	public String getExistingTier() { return existingTier; }
+
+	public void setExistingTier(String existingTier) { this.existingTier = existingTier; }
+
 	public void setApiName(String apiName) {
 		this.apiName = apiName;
 	}
+
+	public String getApiVersion() { return apiVersion; }
+
+	public void setApiVersion(String apiVersion) { this.apiVersion = apiVersion; }
 
 	public String getUser() {
 		return user;
@@ -56,4 +67,8 @@ public class SubscriptionEditDTO {
 	public void setDepartment(String department) {
 		this.department = department;
 	}
+
+	public String getServiceProvider() { return sp; }
+
+	public void setServiceProvider(String sp) { this.sp = sp; }
 }

components/workflow/workflow-service/src/main/java/com/wso2telco/workflow/utils/EmailNotificationUtil.java
@@ -83,6 +83,79 @@ public class EmailNotificationUtil {
 		
 		return content;
 	}
+
+	public static String getAppTierChangeSPEmailNotificationContent(String serviceProvider, String applicationName,
+																	String existingApplicationTier, String newApplicationTier) {
+
+		String content = "";
+
+		content = "<div style='margin: 0 0 3px 0;padding: 5px;border-style: solid;border-width: 1px 1px 1px 11px;border-color:#e46c0a;color: #525863;line-height: normal;font-weight: bold;'>" +
+				"<h2> Application Approval Status</h2>" +
+				"</div>" +
+				"<div style='border-style:solid;border-width:1px 1px 1px 1px;border-color:#e46c0a;color:#525863;'>" +
+				"<table border='0'>" +
+				"<tr>" +
+				"<td><b> Application User : </b></td>" +
+				"<td>" + serviceProvider + "</td>" +
+				"</tr>" +
+				"<tr>" +
+				"<td><b> Application Name : </b></td>" +
+				"<td>" + applicationName + "</td>" +
+				"</tr>" +
+				"<tr>" +
+				"<td><b> Existing Application Tier : </b></td>" +
+				"<td>" + existingApplicationTier + "</td>" +
+				"</tr>" +
+				"<tr>" +
+				"<td><b> New Application Tier : </b></td>" +
+				"<td>" + newApplicationTier + "</td>" +
+				"</tr>" +
+				"</table>" +
+				"</div>";
+
+		return content;
+	}
+
+	public static String getSubscriptionTierChangeSPEmailNotificationContent(String serviceProvider, String apiName, String apiVersion, String applicationName,
+																		String existingSubscriptionTier, String newSubscriptionTier) {
+
+		String content = "";
+
+		content = "<div style='margin: 0 0 3px 0;padding: 5px;border-style: solid;border-width: 1px 1px 1px 11px;border-color:#e46c0a;color: #525863;line-height: normal;font-weight: bold;'>" +
+				"<h2> Subscription Approval Request</h2>" +
+				"</div>" +
+				"<div style='border-style:solid;border-width:1px 1px 1px 1px;border-color:#e46c0a;color:#525863;'>" +
+				"<table border='0'>" +
+				"<tr>" +
+				"<td><b> Application User : </b></td>" +
+				"<td>" + serviceProvider + "</td>" +
+				"</tr>" +
+				"<tr>" +
+				"<td><b> Application Name : </b></td>" +
+				"<td>" + applicationName + "</td>" +
+				"</tr>" +
+				"<tr>" +
+				"<td><b> API Name : </b></td>" +
+				"<td>" + apiName + "</td>" +
+				"</tr>" +
+				"<tr>" +
+				"<td><b> API Version : </b></td>" +
+				"<td>" + apiVersion + "</td>" +
+				"</tr>" +
+				"<tr>" +
+				"<td><b> Existing Subscription Tier : </b></td>" +
+				"<td>" + existingSubscriptionTier + "</td>" +
+				"</tr>" +
+				"<tr>" +
+				"<td><b> New Subscription Tier : </b></td>" +
+				"<td>" + newSubscriptionTier + "</td>" +
+				"</tr>" +
+				"</table>" +
+				"</div>";
+
+		return content;
+
+	}
 	
 	public static String getSubPluginApproverEmailContent(
 			String apiName, String apiVersion, String apiContext, String apiProvider, 

WSO2Telco/component-dep

components/workflow/workflow-service/src/main/java/com/wso2telco/workflow/model/SubscriptionEditDTO.java
@@ -4,10 +4,12 @@ public class SubscriptionEditDTO {
 
 	private Integer applicationId;
 	private String applicationName;
-	private String existingTier;
 	private String subscriptionTier;
+	private String existingTier;
 	private String apiName;
+	private String apiVersion;
 	private String user;
+	private String serviceProvider;
 	private String department;
 	private int apiId;
 
@@ -27,29 +29,27 @@ public class SubscriptionEditDTO {
 		this.applicationName = applicationName;
 	}
 
-	public String getExistingTier() {
-		return existingTier;
-	}
-
-	public void setExistingTier(String existingTier) {
-		this.existingTier = existingTier;
-	}
-
 	public String getSubscriptionTier() {
 		return subscriptionTier;
 	}
 
-	public void setSubscriptionTier(String subscriptionTier) {
-		this.subscriptionTier = subscriptionTier;
-	}
+	public void setSubscriptionTier(String subscriptionTier) { this.subscriptionTier = subscriptionTier; }
 
 	public String getApiName() {
 		return apiName;
 	}
 
+	public String getExistingTier() { return existingTier; }
+
+	public void setExistingTier(String existingTier) { this.existingTier = existingTier; }
+
 	public void setApiName(String apiName) {
 		this.apiName = apiName;
 	}
+
+	public String getApiVersion() { return apiVersion; }
+
+	public void setApiVersion(String apiVersion) { this.apiVersion = apiVersion; }
 
 	public String getUser() {
 		return user;
@@ -74,4 +74,9 @@ public class SubscriptionEditDTO {
     public void setApiID(int apiId) {
         this.apiId = apiId;
     }
+
+	public String getServiceProvider() { return serviceProvider; }
+
+	public void setServiceProvider(String serviceProvider) { this.serviceProvider = serviceProvider; }
+
 }

components/workflow/workflow-service/src/main/java/com/wso2telco/workflow/utils/EmailNotificationUtil.java
@@ -83,6 +83,79 @@ public class EmailNotificationUtil {
 		
 		return content;
 	}
+
+	public static String getAppTierChangeSPEmailNotificationContent(String serviceProvider, String applicationName,
+																	String existingApplicationTier, String newApplicationTier) {
+
+		String content = "";
+
+		content = "<div style='margin: 0 0 3px 0;padding: 5px;border-style: solid;border-width: 1px 1px 1px 11px;border-color:#e46c0a;color: #525863;line-height: normal;font-weight: bold;'>" +
+				"<h2> Application Tier Status</h2>" +
+				"</div>" +
+				"<div style='border-style:solid;border-width:1px 1px 1px 1px;border-color:#e46c0a;color:#525863;'>" +
+				"<table border='0'>" +
+				"<tr>" +
+				"<td><b> Application User : </b></td>" +
+				"<td>" + serviceProvider + "</td>" +
+				"</tr>" +
+				"<tr>" +
+				"<td><b> Application Name : </b></td>" +
+				"<td>" + applicationName + "</td>" +
+				"</tr>" +
+				"<tr>" +
+				"<td><b> Existing Application Tier : </b></td>" +
+				"<td>" + existingApplicationTier + "</td>" +
+				"</tr>" +
+				"<tr>" +
+				"<td><b> New Application Tier : </b></td>" +
+				"<td>" + newApplicationTier + "</td>" +
+				"</tr>" +
+				"</table>" +
+				"</div>";
+
+		return content;
+	}
+
+	public static String getSubscriptionTierChangeSPEmailNotificationContent(String serviceProvider, String apiName, String apiVersion, String applicationName,
+																		String existingSubscriptionTier, String newSubscriptionTier) {
+
+		String content = "";
+
+		content = "<div style='margin: 0 0 3px 0;padding: 5px;border-style: solid;border-width: 1px 1px 1px 11px;border-color:#e46c0a;color: #525863;line-height: normal;font-weight: bold;'>" +
+				"<h2> Subscription Tier Status</h2>" +
+				"</div>" +
+				"<div style='border-style:solid;border-width:1px 1px 1px 1px;border-color:#e46c0a;color:#525863;'>" +
+				"<table border='0'>" +
+				"<tr>" +
+				"<td><b> Application User : </b></td>" +
+				"<td>" + serviceProvider + "</td>" +
+				"</tr>" +
+				"<tr>" +
+				"<td><b> Application Name : </b></td>" +
+				"<td>" + applicationName + "</td>" +
+				"</tr>" +
+				"<tr>" +
+				"<td><b> API Name : </b></td>" +
+				"<td>" + apiName + "</td>" +
+				"</tr>" +
+				"<tr>" +
+				"<td><b> API Version : </b></td>" +
+				"<td>" + apiVersion + "</td>" +
+				"</tr>" +
+				"<tr>" +
+				"<td><b> Existing Subscription Tier : </b></td>" +
+				"<td>" + existingSubscriptionTier + "</td>" +
+				"</tr>" +
+				"<tr>" +
+				"<td><b> New Subscription Tier : </b></td>" +
+				"<td>" + newSubscriptionTier + "</td>" +
+				"</tr>" +
+				"</table>" +
+				"</div>";
+
+		return content;
+
+	}
 	
 	public static String getSubPluginApproverEmailContent(
 			String apiName, String apiVersion, String apiContext, String apiProvider, 