8a63617817240b04238af96941e5640929dc5d9e
chathurabuddi <chathu.buddi@gmail.com>
2020-10-29 07:56:01 +0530

    audit logs added for application tier edit. INTGW-1193

--- components/workflow/workflow-service/src/main/java/com/wso2telco/workflow/service/ApplicationService.java
+++ components/workflow/workflow-service/src/main/java/com/wso2telco/workflow/service/ApplicationService.java
@@ -6,10 +6,12 @@ import com.wso2telco.workflow.dao.ApplicationDAO;
 import com.wso2telco.workflow.model.ApplicationEditDTO;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.wso2.carbon.CarbonConstants;
 
 public class ApplicationService {
 
-	private static Log log = LogFactory.getLog(ApplicationService.class);
+	private static Log LOG = LogFactory.getLog(ApplicationService.class);
+	private static final Log AUDIT_LOG = CarbonConstants.AUDIT_LOG;
 
 	ApplicationDAO applicationDAO;
 
@@ -18,14 +20,19 @@ public class ApplicationService {
 	}
 	
 	public void editApplicationTier(ApplicationEditDTO application) throws SQLException, BusinessException {
-
+		String currentTier = applicationDAO.getSubscriptionTier(application.getApplicationId());
 		applicationDAO.editApplicationTier(application);
-		String logEntry = "Application tier edited :"
-				+ " Completed by - " + application.getUser()
-				+ ", Application name - " + application.getApplicationName()
-				+ ", Previous tier - " + application.getExistingTier()
-				+ ", New tier - " + application.getApplicationTier()
-				+ ", Date & Time - " + applicationDAO.getUpdatedTime(application);
-		log.info(logEntry);
+		this.editApplicationAuditLog(currentTier, application);
+	}
+
+	private void editApplicationAuditLog(String currentTier, ApplicationEditDTO application) {
+		String logMessage = "Application Updated." +
+				" | Application: " + application.getApplicationId() + ":" + application.getExistingTier() +
+				" | SP: " + application.getServiceProvider() +
+				" | Previous Tier: " + currentTier +
+				" | Updated Tier: " + application.getApplicationTier() +
+				" | User: " + application.getUser();
+		AUDIT_LOG.info(logMessage);
+		LOG.info(logMessage);
 	}
 }
